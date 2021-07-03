47332e478118d58f0dee6b2935eb583b36b5c8f2
Aaron Kauffman <aaron.christian.kauffman@gmail.com>
2021-07-02 20:43:16 -0700

    add analyzeHierarchy function

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,36 @@
 //back-end
 function countUp(int) {
-
-    let dummy = [];
+    let dummy = [];//variables for this function
     let base = null;
 
-    while (base < int) {
+    while (base < int) { //while loop to generate array that increments by one toward user defined integer
         base++
         dummy.push(base);
     }
-    return dummy;
+    return dummy; //return newly generated array
 }
 
-function convertToStatement(int){
-    let dummy = [];
-    int.array.forEach(element => {
-        
+function analyzeHierarchy(int) {
+    int.forEach(element => {
+        console.log(element);
+        let hasThree = /3/.test(element);//test if current element of contains either 3,2,1
+        let hasTwo = /2/.test(element);
+        let hasOne = /1/.test(element);
+
+        if (hasThree == true){ //determing what element will be replaced by
+            console.log(element + " = " + "neighbor")
+            //call for element to change neighbor
+        } else {
+            if (hasTwo == true){
+                console.log(element + " = " + "boop")
+                //change element to boop
+            } else {
+                if (hasOne == true){
+                    console.log(element + " = " + "beep")
+                    //change element to beep
+                }
+            }
+        }
     });
 }
 
@@ -25,6 +41,7 @@ $(document).ready(function () {
         //ui logic goes here
         let input = $("input#text").val();
         input = countUp(input);
+        analyzeHierarchy(input);
     });
     // :)
 });
