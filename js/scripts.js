0f95717dc8a77aee0bc70b8330a0f295414f6475
Aaron Kauffman <aaron.christian.kauffman@gmail.com>
2021-07-02 21:22:41 -0700

    add convertToResult function

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,44 @@
 //back-end
-function countUp(int) {
+function countUp(num) {
     let dummy = [];//variables for this function
-    let base = null;
+    let int = 0;
 
-    while (base < int) { //while loop to generate array that increments by one toward user defined integer
-        base++
-        dummy.push(base);
+    while (int <= num) { //while loop to generate array that increments by one toward user defined integer
+        dummy.push(int);
+        int++
     }
     return dummy; //return newly generated array
 }
 
-function analyzeHierarchy(int) {
-    int.forEach(element => {
+function analyzeHierarchy(num) {
+    num.forEach(element => {
         console.log(element);
-        let hasThree = /3/.test(element);//test if current element of contains either 3,2,1
+        let hasThree = /3/.test(element);//test if current element contains either 3,2,1
         let hasTwo = /2/.test(element);
         let hasOne = /1/.test(element);
 
-        if (hasThree == true){ //determing what element will be replaced by
-            console.log(element + " = " + "neighbor")
-            //call for element to change neighbor
+        if (hasThree == true) { //determine what the element will be replaced by
+            console.log(element + " = " + "neighbor") //debug
+            num[element] = "Won't you be my neighbor?";
         } else {
-            if (hasTwo == true){
+            if (hasTwo == true) {
                 console.log(element + " = " + "boop")
-                //change element to boop
+                num[element] = "Boop!";
             } else {
-                if (hasOne == true){
+                if (hasOne == true) {
                     console.log(element + " = " + "beep")
-                    //change element to beep
+                    num[element] = "Beep!";
                 }
             }
         }
     });
+    return num;
+}
+
+function convertToResult(num) {
+    num = num.toString(); //convert array to string
+    num = num.replace(/[, ]+/g, " "); //replace commas with spaces
+    return num
 }
 
 $(document).ready(function () {
@@ -39,9 +46,9 @@ $(document).ready(function () {
     $("form").submit(function () {
         event.preventDefault();
         //ui logic goes here
-        let input = $("input#text").val();
-        input = countUp(input);
-        analyzeHierarchy(input);
+        let int = $("input#text").val();
+        int = countUp(int);
+        $("#result").text(convertToResult(analyzeHierarchy(int)));
     });
     // :)
 });
