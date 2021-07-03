c14efebcf3a43e2dd97db180800ae5be4d6f9ab3
Aaron Kauffman <aaron.christian.kauffman@gmail.com>
2021-07-02 21:54:32 -0700

    clean up code, add styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,27 @@
-//back-end
-function countUp(num) {
+function countUp(num) {//back-end starts here 👇
     let dummy = [];//variables for this function
     let int = 0;
 
     while (int <= num) { //while loop to generate array that increments by one toward user defined integer
         dummy.push(int);
-        int++
+        int++;
     }
-    return dummy; //return newly generated array
+    return dummy;
 }
 
 function analyzeHierarchy(num) {
     num.forEach(element => {
-        console.log(element);
         let hasThree = /3/.test(element);//test if current element contains either 3,2,1
         let hasTwo = /2/.test(element);
         let hasOne = /1/.test(element);
 
         if (hasThree == true) { //determine what the element will be replaced by
-            console.log(element + " = " + "neighbor") //debug
             num[element] = "Won't you be my neighbor?";
         } else {
             if (hasTwo == true) {
-                console.log(element + " = " + "boop")
                 num[element] = "Boop!";
             } else {
                 if (hasOne == true) {
-                    console.log(element + " = " + "beep")
                     num[element] = "Beep!";
                 }
             }
@@ -38,18 +33,14 @@ function analyzeHierarchy(num) {
 function convertToResult(num) {
     num = num.toString(); //convert array to string
     num = num.replace(/[, ]+/g, " "); //replace commas with spaces
-    return num
-}
+    return num;
+} //back-end starts here 👆
 
 $(document).ready(function () {
-    // :)
-    $("form").submit(function () {
+    $("form").submit(function () {//front-end starts here 👇
         event.preventDefault();
-        //ui logic goes here
-        let int = $("input#text").val();
+        let int = $("input#text").val();//get user input
         int = countUp(int);
-        $("#result").text(convertToResult(analyzeHierarchy(int)));
+        $("#result").text(convertToResult(analyzeHierarchy(int)));//analyze array hierarchy, push text to array, display result.
     });
-    // :)
-});
-
+});//front-end ends here 👆
