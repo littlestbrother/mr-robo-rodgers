0da727b56c5060f73236efc4e891db699b8c1839
Aaron Kauffman <aaron.christian.kauffman@gmail.com>
2021-07-02 19:21:12 -0700

    add countUp function

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,31 @@
-$(document).ready(function() {
-console.log("Jquery loaded properly");
+//back-end
+function countUp(int) {
+
+    let dummy = [];
+    let base = null;
+
+    while (base < int) {
+        base++
+        dummy.push(base);
+    }
+    return dummy;
+}
+
+function convertToStatement(int){
+    let dummy = [];
+    int.array.forEach(element => {
+        
+    });
+}
+
+$(document).ready(function () {
+    // :)
+    $("form").submit(function () {
+        event.preventDefault();
+        //ui logic goes here
+        let input = $("input#text").val();
+        input = countUp(input);
+    });
+    // :)
 });
+
